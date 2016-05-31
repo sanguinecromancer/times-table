2fa722538d941c4a5abfc39795075d97808b99f5
necromancer <sanguinecromancer@users.noreply.github.com>
2016-05-31 12:50:05 +0300

    Update ViewController.swift

--- TimesTables/ViewController.swift
+++ TimesTables/ViewController.swift
@@ -1,15 +1,39 @@
 //
 //  ViewController.swift
-//  TimesTables
+//  TimesTables2
 //
-//  Created by Zeynep Aykal on 11/03/16.
+//  Created by Zeynep Aykal on 22/03/16.
 //  Copyright © 2016 Zeynep Aykal. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate {
+ 
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var siderValue: UISlider!
 
+    @IBAction func sliderMoved(sender: AnyObject) {
+        table.reloadData()
+    }
+    
+   func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+   {
+    return 10
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+    let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+    let timesTable = Int(siderValue.value)
+        cell.textLabel?.text = String(timesTable * indexPath.row)
+        return cell
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +46,5 @@ class ViewController: UIViewController {
 
 
 }
+
 
